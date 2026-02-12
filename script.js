8ba54ef077e73bad96fe64e760e6b39f41a9b6ef
SPOORTHI <spoogowda56@gmail.com>
2026-02-12 10:25:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const profileImage = document.getElementById("profileImage");
+
+profileImage.addEventListener("error", () => {
+  profileImage.src = "https://images.unsplash.com/photo-1494790108377-be9c29b29330?auto=format&fit=crop&w=800&q=80";
+});
